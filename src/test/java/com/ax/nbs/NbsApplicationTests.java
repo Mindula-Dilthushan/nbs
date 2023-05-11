4642d21b01fb26afdab6aa9d222101a41e6fd2d0
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2023-05-11 16:46:38 +0530

    nbs-init

---
+++ src/test/java/com/ax/nbs/NbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ax.nbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
